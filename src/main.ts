e55de5ed993329c2fcc9dc0b22978ebbd82bc696
Vito Mannina <mannina.vito@gmail.com>
2025-06-09 14:57:35 +0200

    feat(common): implement HttpExceptionFilter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 import { config } from 'dotenv';
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
